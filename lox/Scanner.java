9e7cecf91937754554c2f8f1592de207610ee51a
sync4thet1c <sauravkshetry444@gmail.com>
2025-04-02 22:03:02 +0545

    4.5.2 Add Multi-Character Lexemes

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -42,6 +42,10 @@ public class Scanner {
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
             case '*': addToken(STAR); break; 
+            case '!': addToken(match('=') ? BANG_EQUAL: BANG); break;
+            case '=': addToken(match('=') ? EQUAL_EQUAL: BANG; break;
+            case '<': addToken(match('=') ? LESS_EQUAL: BANG); break;
+            case '>': addToken(match('=') ? GREATER_EQUAL: BANG); break;
             default:
                 Lox.error(line, "Unexpected token.");
                 break;
@@ -59,6 +63,14 @@ public class Scanner {
     private void addToken(TokenType type, object literal) {
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean match(char expected) {
+        if(isAtEnd()) return false;
+        if(source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
     }
 }
 
