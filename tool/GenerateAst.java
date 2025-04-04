5baf01c191842e16bc66004e32b37fe998c00f3c
sync4thet1c <sauravkshetry444@gmail.com>
2025-04-04 20:49:32 +0545

    add defineAst method to output the base Expr class

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -20,4 +20,19 @@ public class GenerateAst {
             "Unary : Token operator, Expr right"
         ));
     }
+
+    private static void defineAst (
+        String outputDir, String baseName, List<String> types) throws IOException {
+            String path = outputDir + "/" + baseName + ".java";
+            PrintWriter writer = new PrintWriter(path, "UTF-8"); 
+
+            writer.println(".lox;");
+            writer.println();
+            writer.println("import java.util.List;");
+            writer.println();
+            writer.println("abstract class" + baseName + " {");
+
+            writer.println("}");
+            writer.close();
+        }
 }
